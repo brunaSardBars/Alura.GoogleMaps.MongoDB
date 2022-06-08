33db5f5effc212a71bce75a7e676e025f53541e7
brunasa <brunasa@synnex.com>
2022-06-08 19:12:48 -0300

    Add project files.

---
+++ Alura.GoogleMaps.Web/Models/Aeroporto.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Alura.GoogleMaps.Web.Models
+{
+    public class Aeroporto
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates> loc { get; set; }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public string code { get; set; }
+
+    }
+}
---
+++ Alura.GoogleMaps.Web/Models/ConnectionMongoDB.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Alura.GoogleMaps.Web.Models
+{
+    public class ConnectionMongoDB
+    {
+        public const string CONNECTION_STRING = "mongodb://127.0.0.1:27017";
+        public const string DATABASE = "geo";
+        public const string COLLECTION = "airports";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static ConnectionMongoDB()
+        {
+            _client = new MongoClient(CONNECTION_STRING);
+            _database = _client.GetDatabase(DATABASE);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Aeroporto> Airports
+        {
+            get { return _database.GetCollection<Aeroporto>(COLLECTION); }
+        }
+
+
+    }
+}
